8285811d3c239ebff5c90f230392dd081759aa22
Muhammad Arif Ali Hakim <muharif1203@gmail.com>
2024-12-23 20:17:52 +0700

    Update

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'form', 'function', 'security', 'captcha');
+$autoload['helper'] = array('url', 'form', 'function', 'security');
 
 /*
 | -------------------------------------------------------------------
